7149a3e84479857adbe7477bdf51d4b426b73985
WxyDZDgn <279916012@qq.com>
2026-04-19 14:50:33 +0800

    [未完成] 进一步修改parser逻辑, 顺便添加其中一些错误

--- exer/parser.py
+++ exer/parser.py
@@ -1,4 +1,11 @@
-from unit.token import FuncEml, IdentVariable, Assignment, EndOfStmt
+from unit.token import (
+    FuncEml,
+    IdentVariable,
+    Assignment,
+    EndOfStmt,
+    WhiteSpace,
+    Annotation,
+)
 from unit.node import _Node
 from exer.lexer import lexer
 
@@ -20,6 +27,8 @@ class Parser:
         # 暂时不支持赋值
         end_of_stmt_indexes = []
         assignment_indexes = []
+        left_index = 0
+        cur_assignment_indexes_index = 0
         for index in range(len(tokens)):
             cur = tokens[index]
             match cur:
@@ -28,9 +37,30 @@ class Parser:
                 case _ if isinstance(cur, Assignment):
                     assignment_indexes.append(index)
 
-        print(tokens)
-        print(end_of_stmt_indexes)
-        print(assignment_indexes)
+        for right_index in end_of_stmt_indexes:
+            has_assignments_in_cur_stmt = False
+            for i in range(cur_assignment_indexes_index, len(assignment_indexes)):
+                if assignment_indexes[i] < right_index:
+                    if has_assignments_in_cur_stmt:
+                        assert False, "同一个语句中不支持多个'='"
+                    has_assignments_in_cur_stmt = True
+                elif assignment_indexes[i] > right_index:
+                    break
+                else:
+                    assert False, "不期望的分支"
+
+            # TODO: 还没结束
+
+            # 结束部分
+            left_index = right_index + 1
+            if has_assignments_in_cur_stmt:
+                cur_assignment_indexes_index += 1
+
+        for index in range(left_index, len(tokens)):
+            cur = tokens[index]
+            if isinstance(cur, WhiteSpace) or isinstance(cur, Annotation):
+                continue
+            assert False, "未完成的Stmt"
         return []
 
     pass
