25d6fa45ac5febbd6867184d2b91fadf5475cefd
WxyDZDgn <279916012@qq.com>
2026-04-21 22:52:42 +0800

    增加repr便于调试

--- unit/node.py
+++ unit/node.py
@@ -6,10 +6,11 @@ from typing import Self
 class _Node:
     def __init__(self, token: _Token, param_count: int = 0) -> None:
         self.token: _Token = token
-        self.param_count: int = param_count
         self.params: list[_Node] = []
 
     def append(self, node: Self) -> None:
-        if len(self.params) >= self.param_count:
-            assert False
         self.params.append(node)
+
+    def __repr__(self) -> str:
+        return f"<Node: '{str(self.token)}' [{', '.join(list(map(str, self.params)))}]>"
+    
