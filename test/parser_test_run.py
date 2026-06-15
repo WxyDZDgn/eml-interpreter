dd1aa83934f005220f77cdbef1f1a56521cc4fcd
WxyDZDgn <279916012@qq.com>
2026-06-15 22:03:21 +0800

    清除示例结果

--- test/parser_test_run.py
+++ test/parser_test_run.py
@@ -4,50 +4,7 @@
 from exer.parser import parser
 
 if __name__ == "__main__":
-    code = """a(b());"""
+    code = """e(x) = eml(x, 1);
+ln(x) = eml(1, eml(eml(1, x), 1));"""
     nodes = parser(code)
     print(nodes)
-
-"""
-e(x) = eml(x, 1);
-ln(x) = eml(1, eml(eml(1, x), 1));
-
-[
-    <Node: '<Assignment: '='>' [
-        <Node: '<IdentVariable: 'e'>' [
-            <Node: '<IdentVariable: 'x'>' [
-            ]>
-        ]>, 
-        <Node: '<IdentVariable: 'eml'>' [
-            <Node: '<IdentVariable: 'x'>' [
-            ]>, 
-            <Node: '<ConstInt: '1'>' [
-            ]>
-        ]>
-    ]>, 
-    <Node: '<Assignment: '='>' [
-        <Node: '<IdentVariable: 'ln'>' [
-            <Node: '<IdentVariable: 'x'>' [
-            ]>
-        ]>, 
-        <Node: '<IdentVariable: 'eml'>' [
-            <Node: '<IdentVariable: 'eml'>' [
-                <Node: '<IdentVariable: 'eml'>' [
-                    <Node: '<ConstInt: '1'>' [
-                    ]>, 
-                    <Node: '<IdentVariable: 'x'>' [
-                    ]>
-                ]>, 
-                <Node: '<IdentVariable: 'eml'>' [
-                ]>, 
-                <Node: '<ConstInt: '1'>' [
-                ]>
-            ]>, 
-            <Node: '<ConstInt: '1'>' [
-            ]>, 
-            <Node: '<IdentVariable: 'eml'>' [
-            ]>
-        ]>
-    ]>
-]
-"""
