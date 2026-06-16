7430cf404ca8393ae34a3db9f445b595497a4299
WxyDZDgn <279916012@qq.com>
2026-06-16 12:03:46 +0800

    重构`symbol_table`
    将`ident_name`改为`ident_token`以存储附加信息
    
    添加重载`in`

--- unit/symbol_table.py
+++ unit/symbol_table.py
@@ -4,6 +4,7 @@
 from typing import Optional
 
 from unit.node import Node
+from unit.token import Token
 
 
 class SymbolTable:
@@ -17,30 +18,35 @@ class SymbolTable:
 
     # 不确定 ident_name 要不要用 Token 或者 Node 代替，后者甚至包括 parameter_count
     def __init__(self):
-        self.table: dict[tuple[str, int], tuple[bool, bool, Optional[Node]]] = dict()
+        self.table: dict[tuple[Token, int], tuple[bool, bool, Optional[Node]]] = dict()
 
-    def is_defined(self, ident_name: str, parameter_count: int) -> bool:
-        tmp = (ident_name, parameter_count)
-        return tmp in self.table.keys() and self.table[tmp][0]
+    def is_defined(self, ident_token: Token, parameter_count: int) -> bool:
+        tmp = (ident_token, parameter_count)
+        return tmp in self.table and self.table[tmp][0]
 
-    def is_modifiable(self, ident_name: str, parameter_count: int) -> bool:
-        tmp = (ident_name, parameter_count)
+    def is_modifiable(self, ident_token: Token, parameter_count: int) -> bool:
+        tmp = (ident_token, parameter_count)
         return self.is_defined(*tmp) and self.table[tmp][1]
 
     def clear(self):
         # 本打算直接修改有效位的，鉴于元组无法修改，并且没想好显式指定类型到定长可修改结构，先暂时 `.clear()`
         self.table.clear()
 
-    def put(self, ident_name: str, parameter_count: int, is_modifiable: bool = True,
+    def put(self, ident_token: Token, parameter_count: int, is_modifiable: bool = True,
             ast_node: Optional[Node] = None) -> bool:
-        if self.is_defined(ident_name, parameter_count) and not self.is_modifiable(ident_name, parameter_count):
-            return False
-        tmp = (ident_name, parameter_count)
-        assert tmp not in self.table.keys() or self.table[tmp][1] == is_modifiable
+        tmp = (ident_token, parameter_count)
+        if self.is_defined(*tmp):
+            if not self.is_modifiable(*tmp):
+                return False
+            assert is_modifiable == self.is_modifiable(*tmp)
         self.table[tmp] = (True, is_modifiable, ast_node)
         return True
 
-    def get(self, ident_name: str, parameter_count: int) -> Optional[Node]:
-        if not self.is_defined(ident_name, parameter_count):
+    def get(self, ident_token: Token, parameter_count: int) -> Optional[Node]:
+        if not self.is_defined(ident_token, parameter_count):
             return None
-        return self.table[(ident_name, parameter_count)][2]
+        return self.table[(ident_token, parameter_count)][2]
+
+    def __contains__(self, item: Node) -> bool:
+        token, count = item.token, len(item.params)
+        return (token, count) in self.table
